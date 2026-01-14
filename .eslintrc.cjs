d03ca88f22a9ebbd153c5021f9672465473a5258
Adrián Mariscal <36743254+AdriMariscal@users.noreply.github.com>
2026-01-14 11:24:22 +0100

    Allow pnpm install without frozen lockfile

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+  rules: {
+    semi: 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
