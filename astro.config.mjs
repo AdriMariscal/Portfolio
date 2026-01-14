74ad58849990bd45c25d94168faee2980b9d4521
Adrián Mariscal <36743254+AdriMariscal@users.noreply.github.com>
2026-01-14 16:25:41 +0100

    Fix sitemap filter for staging (#239)

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,7 +38,7 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap({
-      filter: (page) => !page.pathname.startsWith('/staging/'),
+      filter: (page) => !page.pathname?.startsWith('/staging/'),
     }),
   ],
 
