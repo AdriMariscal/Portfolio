cc7dbc81abbac31e624b54a89fe99d91ba7089b4
Adrián Mariscal <36743254+AdriMariscal@users.noreply.github.com>
2026-01-15 15:10:15 +0100

    Trim Astro lint overrides

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import astro from "eslint-plugin-astro";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import astroParser from "astro-eslint-parser";
 
 const sharedGlobals = {
   window: "readonly",
@@ -49,19 +48,6 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
       parser: tsParser,
-      globals: sharedGlobals,
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      ...sharedRules,
-    },
-  },
-  {
-    files: ["**/*.astro"],
-    languageOptions: {
       globals: sharedGlobals,
     },
     plugins: {
